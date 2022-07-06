5e59f2beaf227efbe9476f0ea844a9ca84a77574
dataninja-python <aj@redstonema.com>
2022-07-06 15:42:54 -0400

    can successfully update dom with new timer info

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,82 +1,32 @@
-const main = () => {
-	console.log("loaded from external javascript file");
+const main = (event) => {
+	console.log("main function started");
 
-	// Interface Elements
-	const timeElement = document.getElementById("time-display");
-	const amPmElement = document.getElementById("ampm");
+	// set constants
+	const startFocusMinutes = 25;
+	const startRestMinutes = 5;
+	const startSeconds = 60;
+	const states = ["Focus", "Rest", "Waiting"];
+	let currentMinutes;
+	let currentSeconds;
+	let currentState = "";
 
-	const amString = "am";
-	const pmString = "pm";
+	let stateElement = document.querySelector("#status-state");
+	let minutesElement = document.querySelector("#min-counter");
+	let secondsElement = document.querySelector("#sec-counter");
 
-	// Utility Functions
-	const zeroPaddNumer = (unPaddedNumber, paddedLength) => {
-		return String(unPaddedNumber).padStart(paddedLength, "0");
-	};
+	stateElement.innerHTML = "rest";
+	minutesElement.innerHTML = String(23);
+	secondsElement.innerHTML = String(53);
 
-	const currentTime = () => {
-		return new Date();
-	};
-
-	const hoursValue = (theTime) => {
-		let rightNow = new Date();
-		let rawHours = rightNow.getHours();
-		if (rawHours == 0) {
-			return 12;
-		} else if (rawHours > 12) {
-			return rawHours - 12;
-		} else {
-			return rawHours;
-		}
-	};
-
-	const minutesValue = (theTime) => {
-		let rightNow = new Date();
-		let rawMinutes = rightNow.getMinutes();
-		if(rawMinutes < 10) {
-			return zeroPaddedNumber(rawMinutes, 2);
-		} else {
-			return rawMinutes;
-		}
-	};
-
-	const secondsValue = (theTime) => {
-		let rightNow = new Date();
-		let rawSeconds = rightNow.getSeconds();
-		if(rawSeconds < 10) {
-			return zeroPaddedNumber(rawSeconds, 2);
-		} else {
-			return rawSeconds;
-		}
-	};
-
-	const amPmValue = (theTime) => {
-		let rightNow = new Date();
-		let rightNowHoursNumber = rightNow.getHours();
-
-		if (rightNowHoursNumber >= 12) { 
-			return pmString; 
-		} else {
-			return amString;
-		}
-	};
-
-	const updateTimeValues = () => {
-		let rightNow = currentTime();
-		let currentTimeString = `${hoursValue(rightNow)}:${minutesValue(rightNow)}:${secondsValue(rightNow)}`;
-		let currentAmPmString = `${amPmValue(rightNow)}`;
-
-		timeElement.innerHTML = currentTimeString;
-		amPmElement.innerHTML = currentAmPmString;
-
-		let dateTimeString = `${rightNow.getHours()}:${rightNow.getMinutes()}:${rightNow.getSeconds()}`;
-		timeElement.setAttribute("datetime", `${dateTimeString}`);
-	};
-
-	setInterval(() => { updateTimeValues(); }, 1000);
+	// quick pseudocode
+	// set everything up if first time
+	// clean everything up if end
+	
 };
 
 
-window.addEventListener("DOMContentLoaded", (event) => {
-	console.log("DOM loaded");
-	main();
+
+window.addEventListener("load", (event) => {
+	console.log("full page and assets loaded and ready");
+	main(event);
 });
